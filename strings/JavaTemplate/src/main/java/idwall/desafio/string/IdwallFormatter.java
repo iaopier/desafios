ccaf8243aa9f0510de86a1e210ea4f3f062ff622
iaopier <pierhett@gmail.com>
2019-04-18 18:36:35 -0300

    Teste inserido - corrigindo método de formatação da String.

--- strings/JavaTemplate/src/main/java/idwall/desafio/string/IdwallFormatter.java
+++ strings/JavaTemplate/src/main/java/idwall/desafio/string/IdwallFormatter.java
@@ -13,6 +13,23 @@ public class IdwallFormatter extends StringFormatter {
      */
     @Override
     public String format(String text) {
+        String output = "";
+        int countChar = 0;
+        int limit = getLimit();
+        for (; countChar < text.length(); ++countChar) {
+            if ((countChar % getLimit()) == 0 && limit < text.length()) {
+                if (text.charAt(limit) == ' ') {
+                    output = output + text.substring(countChar, limit) + "\n";
+                    limit += getLimit();
+                }else{
+                    output = output + text.substring(countChar, limit) + "\n";
+                    limit += getLimit();
+                }
+
+            }
+        }
+        System.out.println(output);
         throw new UnsupportedOperationException();
     }
+
 }
